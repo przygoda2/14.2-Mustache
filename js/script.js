88a38b2a4ad94e2962afa737967b014753f823e2
Piotr Przygoda <przygoda@gmail.com>
2019-02-13 23:54:41 +0100

    Project copy 14.1 beginning of project implementation 14.2

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// flickity
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity(elem, {
+  cellAlign: 'center',
+  contain: true,
+  hash: true,
+  pageDots: false
+});
+
+  // button reset
+  var btnRestart = document.querySelector('.btn-restart');
+  btnRestart.addEventListener('click', function(){
+    flkty.select(0);
+  });
+
+  // scroll
+  var progressBar = document.querySelector('.progress-bar');
+  
+  flkty.on( 'scroll', function( progress ) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
+  });
