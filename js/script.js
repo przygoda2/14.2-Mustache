d8cf1096381f9eca220b63865ef2506ec5abcb60
Piotr Przygoda <przygoda@gmail.com>
2019-02-14 23:55:13 +0100

    Completeted project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+var templateSlide = document.getElementById('templateCarousel').innerHTML;
+var carousel = document.querySelector('.main-carousel');
+
+Mustache.parse(templateSlide);
+var renderedTemplates = '';
+
+for (var i = 0; i < data.length; i++) {
+  renderedTemplates += Mustache.render(templateSlide, data[i]);
+}
+carousel.innerHTML = renderedTemplates;
+
 // flickity
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity(elem, {
@@ -7,16 +18,16 @@ var flkty = new Flickity(elem, {
   pageDots: false
 });
 
-  // button reset
-  var btnRestart = document.querySelector('.btn-restart');
-  btnRestart.addEventListener('click', function(){
-    flkty.select(0);
-  });
+// button reset
+var btnRestart = document.querySelector('.btn-restart');
+btnRestart.addEventListener('click', function () {
+  flkty.select(0);
+});
 
-  // scroll
-  var progressBar = document.querySelector('.progress-bar');
-  
-  flkty.on( 'scroll', function( progress ) {
-    progress = Math.max( 0, Math.min( 1, progress ) );
-    progressBar.style.width = progress * 100 + '%';
-  });
+// scroll
+var progressBar = document.querySelector('.progress-bar');
+
+flkty.on('scroll', function (progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + '%';
+});
